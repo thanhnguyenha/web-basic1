b910fecf3c36725b45005ef626eb47858f59ff4a
thanhnh89 <thanhday@gmail.com>
2026-08-07 12:15:42 +0700

    rebuild

--- _next/static/1TLYM9Stk8LgruHlGWPTn/_buildManifest.js
+++ _next/static/1TLYM9Stk8LgruHlGWPTn/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/web-basic1//_next/:path+",
+        "destination": "/web-basic1/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
